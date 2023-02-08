c0886547a108c06d4ca16d5ebf951412e301d6ae
Andy Woodward <andy.woodward1967@gmail.com>
2023-02-08 12:16:10 +0000

    Add a function to display the heart at a random position and with a random color

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 const body = document.querySelector('body')
+
+class Heart {
+  constructor(color, position, speed) {
+    this.color = color
+    this.position = position
+    this.speed = speed
+  }
+  draw() {
+    const divElement = document.createElement('div')
+    divElement.classList.add('heart')
+    divElement.style.left = this.position + 'px'
+    divElement.style.setProperty('--c', this.color)
+    body.append(divElement)
+  }
+}
+
+// const redHeart = new Heart('red', 10, 200)
+// console.log(redHeart)
+
+function addHeart() {
+  const randomColor =
+    '#' + Math.floor(Math.random() * 16777215).toString(16)
+  const randomLeft = Math.floor(Math.random() * window.innerWidth)
+  const randomSpeed = Math.floor(Math.random() * 500)
+
+  const newHeart = new Heart(randomColor, randomLeft, randomSpeed)
+  console.log(newHeart)
+  newHeart.draw()
+}
+
+addHeart()
